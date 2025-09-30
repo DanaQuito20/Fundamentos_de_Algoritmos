68166fa028fe7f29a3a30671c3c83d7c516cbad5
Dan Quito <dquito@gmail.com>
2025-09-30 09:05:26 -0500

    Actualización

---
+++ Semana 7/Semana7_LA/Semana7_LA.py
@@ -0,0 +1,23 @@
+#Utilizando For
+for i in range (1, 11):
+  print(i, end = " ")
+
+#Utilizando While
+j = 0
+while j < 10:
+  print(j, end = " ")
+  j = j + 1      #j += 1
+
+
+
+
+
+#Utilizando For
+for i in range (10, 0, -1):
+  print(i, end = " ")
+
+#Utilizando While
+j = 10
+while (j >= 1):
+  print(j, end = " ")
+  j = j - 1     #j -= 1
